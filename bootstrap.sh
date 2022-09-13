89d2d5ce1d941e59237d7e82c296287f87692158
pa3datka <pa3datka@gmail.com>
2022-09-13 19:35:41 +0300

    upgrade bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,7 @@
+sudo apt-get update && sudo apt-get install -y \
+  git unzip pip sshpass curl\
+  php7.4-cli php7.4-xml php7.4-tokenizer php7.4-simplexml php7.4-xmlwriter
+
 # copy ssh-key in vagrant
 
 cp /vagrant/ssh-keys/owner.id_rsa ~/.ssh/owner.id_rsa
